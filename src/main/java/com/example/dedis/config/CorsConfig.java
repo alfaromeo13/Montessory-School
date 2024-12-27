b4f1c5d447473ea26940f64c442d466e6ba4dce8
alfaromeo13 <jovanvukovic09@gmail.com>
2024-12-27 14:55:15 +0100

    Fix production bugs

--- src/main/java/com/example/dedis/config/CorsConfig.java
+++ src/main/java/com/example/dedis/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.dedis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${application.frontend.render-link}")
+    private String frontendLink;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("*") // todo: Restrict to calls from render.com PUT FRONTEND LINK
+                .allowedOrigins(frontendLink)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // Restrict HTTP methods
                 .allowedOrigins("*")
                 .exposedHeaders("*");
